c4ce860aeb12985bf1ceb4f7feebb2ebb9cb904f
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2023-12-07 18:33:49 +0400

    initial commit

---
+++ GithubUserSearch/GithubUserSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUserSearchApp.swift
+//  GithubUserSearch
+//
+//  Created by Saba Gogrichiani on 07.12.23.
+//
+
+import SwiftUI
+
+@main
+struct GithubUserSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
